beeb9f5b251a82da93c4160c44ee185d798ba9d4
Reubotimus <reubencook555@gmail.com>
2024-08-05 11:58:14 +1000

    added styling to website

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const router = require('./routers/indexRouter.js')
 
 
 const app = express();
+router.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', router);
 
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {addMessage, getMessages, getForm} = require('../controlers/indexController.js');
 
-
-// router.use(express.json())
-router.use(express.urlencoded({extended: false}))
-
 router.get('/new', getForm)
 router.post('/new', addMessage);
 router.post('/new', (req, res) => res.send("hello"));
